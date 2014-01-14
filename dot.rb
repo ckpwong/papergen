d06d8343cc5e27bdcb6c120a40ce8888e6d2f13c
Cindy Wong <ckpwong@gmail.com>
2014-01-14 10:41:54 -0500

    Extracted into function 2_up_dots

--- dot.rb
+++ dot.rb
@@ -2,30 +2,25 @@
 
 require 'prawn'
 
-# Measurement unit: pt = 1/72"
-# Default paper size:  US Letter by default = 612 x 792
-# Change margin to 1/4" = 18 pt
-
-page_width = 612
-page_height = 792
-margin = 72/4
-space = 10
-colour = "AAAAAA"
-
-Prawn::Document.generate("dot.pdf", :margin => 18) do
-
-	# make it 2-up: y becomes 792/2 = 396 per page
-	x = 0
-	while x <= (page_width - 2 * margin)
-		y = 0
-		z = page_height / 2
-		while y <= (page_height / 2 - 2 * margin) do
-			fill_color colour
-			fill_circle [x, y], 0.5
-			fill_circle [x, z], 0.5
-			y += space
-			z += space
+# generate dot grid in 2-up position, folding at half-height
+def 2_up_dots (filename, paper_width = 612, paper_height = 792, margin = 72/4, space = 10, colour = "DDDDDD", radius = 0.75)
+	half_height = paper_height / 2
+	Prawn::Document.generate(filename, :page_size => [paper_width, paper_height], :margin => margin) do
+		x = 0
+		while x <= (paper_width - 2 * margin) do
+			y = 0
+			z = page_height / 2
+			while y <= (page_height / 2 - 2 * margin) do
+				fill_color colour
+				fill_circle [x, y], 0.5
+				fill_circle [x, z], 0.5
+				y += space
+				z += space
+			end
+			x += space
 		end
-		x += space
 	end
 end
+
+
+2_up_dots "dot2.pdf"

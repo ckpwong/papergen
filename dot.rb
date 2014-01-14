e0212656a5a3f1b57112fbb3ed225a3f33550efe
Cindy Wong <ckpwong@gmail.com>
2014-01-14 10:37:15 -0500

    Extracted exact values to constants

--- dot.rb
+++ dot.rb
@@ -6,20 +6,26 @@ require 'prawn'
 # Default paper size:  US Letter by default = 612 x 792
 # Change margin to 1/4" = 18 pt
 
+page_width = 612
+page_height = 792
+margin = 72/4
+space = 10
+colour = "AAAAAA"
+
 Prawn::Document.generate("dot.pdf", :margin => 18) do
 
 	# make it 2-up: y becomes 792/2 = 396 per page
 	x = 0
-	while x <= (612 - 36) do
+	while x <= (page_width - 2 * margin)
 		y = 0
-		z = 396
-		while y <= (396 - 36) do
-			fill_color "AAAAAA"
+		z = page_height / 2
+		while y <= (page_height / 2 - 2 * margin) do
+			fill_color colour
 			fill_circle [x, y], 0.5
 			fill_circle [x, z], 0.5
-			y += 10
-			z += 10
+			y += space
+			z += space
 		end
-		x += 10
+		x += space
 	end
 end

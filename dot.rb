5ca8216f9c0541c68fa4939291904320da659a48
Cindy Wong <ckpwong@gmail.com>
2014-01-14 10:29:44 -0500

    Make it a grid

--- dot.rb
+++ dot.rb
@@ -3,6 +3,16 @@
 require 'prawn'
 
 Prawn::Document.generate("dot.pdf") do
-	fill_color "AAAAAA"
-	fill_circle [100, 100], 1
+	# Measurement unit: pt = 1/72"
+	# Paper size:  US Letter by default = 612 x 792
+	# Margin: 0.5" = 36 pt
+
+	x = 0
+	while x <= (612 - 72) do
+		y = 0
+		while y <= (792 - 72) do
+			fill_color "AAAAAA"
+			fill_circle [x, y], 1
+		end
+	end
 end

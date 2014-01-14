0a907abb8db951068810fd76a4f3b433e68bcee1
Cindy Wong <ckpwong@gmail.com>
2014-01-14 10:52:10 -0500

    Extracted drawing in each box into separate function; added horizontal rule support

--- dot.rb
+++ dot.rb
@@ -2,29 +2,8 @@
 
 require 'prawn'
 
-# generate dot grid in 2-up position, folding at half-height
-def dots_2_up (filename, paper_width = 612, paper_height = 792, margin = 72/4, space = 10, colour = "DDDDDD", radius = 0.75)
-	half_height = paper_height / 2
-	Prawn::Document.generate(filename, :page_size => [paper_width, paper_height], :margin => margin) do
-		x = 0
-		while x <= (paper_width - 2 * margin) do
-			y = 0
-			z = half_height
-			while y <= (half_height / 2 - 2 * margin) do
-				fill_color colour
-				fill_circle [x, y], 0.5
-				fill_circle [x, z], 0.5
-				y += space
-				z += space
-			end
-			x += space
-		end
-	end
-end
-
-# generalize into n-up, defined by how is the page divided horizontally and vertically
-def n_up_dots(file_name, horizontal = 1, vertical = 2, page_width = 612, page_height = 792, margin = 72/4, pages = 1, radius = 0.75, space = 13.5, color = "DDDDDD", debug = false) 
-	# define boundaries for each mini-page
+# generalized
+def n_up(file_name, page_width, page_height, margin, pages, horizontal, vertical, weight, space, color, draw_func, debug = false) 
 	box_width = page_width / horizontal
 	box_height = page_height / vertical
 	bound_width = box_width - 2 * margin
@@ -54,24 +33,48 @@ def n_up_dots(file_name, horizontal = 1, vertical = 2, page_width = 612, page_he
 
 	Prawn::Document.generate(file_name, :page_size => [page_width, page_height], :margin => margin) do
 		for p in (1 .. pages) do
-			for box in boxes do
-				x = box[:min_x]
-				while x <= box[:max_x] do
-					y = box[:min_y]
-					while y <= box[:max_y] do
-						fill_color color
-						fill_circle [x, y], radius
-						p "dot [#{x}, #{y}]" if debug
-						y += space
-					end
-					x += space
-				end
+			for b in boxes do
+				send draw_func, b, color, weight, space, debug
 			end
 			start_new_page unless p == pages
 		end
 	end
 end
 
+def draw_dots (box, color, weight, space, debug = false)
+	x = box[:min_x]
+	while x <= box[:max_x] do
+		y = box[:min_y]
+		while y <= box[:max_y] do
+			fill_color color
+			fill_circle [x, y], weight
+			p "dot [#{x}, #{y}]" if debug
+			y += space
+		end
+		x += space
+	end
+end
 
-# dots_2_up "dot2.pdf"
+def draw_horizontal_rule (box, color, weight, space, debug = false)
+	y = box[:min_y]
+	while y <= box[:max_y] do
+		stroke_color color
+		stroke do
+			line [box[:min_x], y], [box[:max_x], y]
+			p "horizontal line: [#{box[:min_x]}, #{y}] -> [#{box[:max_x]}, #{y}]" if debug
+		end
+		y += space
+	end
+end
+
+def n_up_horizontal_rule(file_name, horizontal = 1, vertical = 2, page_width = 612, page_height = 792, margin = 72/4, pages = 1, line_width = 0.75, space = 13.5, color = "DDDDDD", debug = false) 
+	n_up file_name, page_width, page_height, margin, pages, horizontal, vertical, line_width, space, color, :draw_horizontal_rule, debug
+end
+
+def n_up_dots(file_name, horizontal = 1, vertical = 2, page_width = 612, page_height = 792, margin = 72/4, pages = 1, radius = 0.75, space = 13.5, color = "DDDDDD", debug = false) 
+	n_up file_name, page_width, page_height, margin, pages, horizontal, vertical, radius, space, color, :draw_dots, debug
+end
+
 n_up_dots "2updots.pdf"
+n_up_horizontal_rule "2uphlines.pdf"
+n_up_dots "4updots.pdf", 2, 2
